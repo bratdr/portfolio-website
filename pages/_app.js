8e78c3ff3bc43c890cd2cbeed528699ea20cc670
Brata Darmawan <bratadarmawan@gmail.com>
2023-01-14 05:56:34 +0700

    Update _app.js
    apply Public_Sans as global fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { Public_Sans } from "@next/font/google";
+
+const ps = Public_Sans({
+  subsets: ["latin"],
+  variable: "--font-ps",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${ps.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
