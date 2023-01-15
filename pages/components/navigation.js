67425a2c225a2a1fc8817824065f2342a0066a22
Brata Darmawan <bratadarmawan@gmail.com>
2023-01-16 04:17:04 +0700

    Update navigation.js
    adding FaRegPaperPlane as a mailto icon link

--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -1,29 +1,41 @@
-import Link from "next/link";
 import React from "react";
-import { FaGithub, FaTelegram, FaLinkedin } from "react-icons/fa";
-import { MdMenu } from "react-icons/md";
+import Link from "next/link";
+import {
+  FaGithub,
+  FaTelegram,
+  FaLinkedin,
+  FaRegPaperPlane,
+} from "react-icons/fa";
 
-const navigation = () => {
+const Navigation = () => {
   return (
-    <div className="fixed top-0 z-10 flex h-16 w-full flex-row items-center justify-between bg-white px-6 shadow-sm lg:px-36">
+    // <div className="fixed top-0 z-10 flex h-16 w-full flex-row items-center justify-between bg-white px-6 shadow-sm lg:px-36">
+    <div className="fixed top-0 flex h-16 w-screen flex-row items-center justify-between bg-[#0c0c0c] px-6 shadow-sm md:z-10 lg:px-36">
       <h1 className="bg-gradient-to-r from-[#F69C15] to-[#F20BBF] bg-clip-text text-lg font-bold tracking-[-0.03em] text-transparent xl:text-2xl">
         Portfolio
       </h1>
-      <div className="sm:hidden">
-        <MdMenu size="24" />
+
+      {/* Mailto */}
+      <div className="text-white active:text-[#F69C15] sm:hidden">
+        <Link href="mailto:work.brata@gmail.com?subject=Hello">
+          <FaRegPaperPlane size="16" />
+        </Link>
       </div>
-      <ul className="hidden flex-row items-center justify-center gap-6 sm:flex">
-        <li className="cursor-pointer">
+
+      {/* Social Platform Logo */}
+      {/* <ul className="hidden flex-row items-center justify-center gap-6 sm:flex"> */}
+      <ul className="hidden flex-row items-center justify-center gap-6 text-white sm:flex">
+        <li className="cursor-pointer hover:text-purple-800">
           <Link href="https://github.com/bratdr">
             <FaGithub size="24" />
           </Link>
         </li>
-        <li>
+        <li className="cursor-pointer hover:text-sky-500">
           <Link href="https://t.me/bratdr">
             <FaTelegram size="24" />
           </Link>
         </li>
-        <li>
+        <li className="cursor-pointer hover:text-blue-700">
           <Link href="https://www.linkedin.com/in/bratadarmawan/">
             <FaLinkedin size="24" />
           </Link>
@@ -33,4 +45,4 @@ const navigation = () => {
   );
 };
 
-export default navigation;
+export default Navigation;
